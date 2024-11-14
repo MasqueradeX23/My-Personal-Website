f7a7edd84379a394981c4b46afa6f4f4a32379d7
Masquerade <oyunplastik@gmail.com>
2024-11-15 00:45:15 +0300

    Yerel değişiklikler

--- js/script.js
+++ js/script.js
@@ -11,3 +11,33 @@ document.querySelector(".contact-form").addEventListener("submit", function(even
     "positionClass": "toast-top-full-width", // Bildirimi üst kısımda tam genişlikte gösterir
     "timeOut": "3000" // Bildirimin 3 saniye sonra kaybolması
   };
+  /**Navbar - Kimdir? - Öğrendiklerim */
+  document.querySelector('a[href="#learning"]').addEventListener('click', function(e) {
+    e.preventDefault(); // Varsayılan davranışı engelle.
+    
+    // Öğrendiklerim kısmına kaydırma
+    const target = document.querySelector('#learning');
+    const offset = 100; // Navbar yüksekliğine göre ayarlayın (örneğin 100px)
+    const elementPosition = target.getBoundingClientRect().top;
+    const offsetPosition = elementPosition - offset;
+    
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: 'smooth'
+    });
+  });
+/**Navbar - İletişim */
+document.querySelector('a[href="#contact"]').addEventListener('click', function(e) {
+  e.preventDefault(); // Varsayılan davranışı engelle.
+  
+  // Öğrendiklerim kısmına kaydırma
+  const target = document.querySelector('#contact');
+  const offset = 80; // Navbar yüksekliğine göre ayarlayın (örneğin 100px)
+  const elementPosition = target.getBoundingClientRect().top;
+  const offsetPosition = elementPosition - offset;
+  
+  window.scrollBy({
+    top: offsetPosition,
+    behavior: 'smooth'
+  });
+});
